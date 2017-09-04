ab2c0ec5ee2e06f08bc683aa8d54c3dafdc83699
balintvecsey <balint.vecsey@gmail.com>
2017-09-04 14:19:25 +0200

    add content container with static data

--- frontend.js
+++ frontend.js
@@ -7,21 +7,35 @@ let contentContainer = document.createElement('div');
 let voteCounter = document.createElement('p');
 let upvoteButton = document.createElement('img');
 let downvoteButton = document.createElement('img');
-let score = 0;
+let contentText = document.createElement('a');
+let submittedText = document.createElement('p');
+let modifyButton = document.createElement('p');
+let deleteButton = document.createElement('p');
 
 voteContainer.className = 'vote container';
 contentContainer.className = 'content container';
 voteCounter.className = 'votecounter';
 upvoteButton.src = 'assets/upvote.png';
 downvoteButton.src = 'assets/downvote.png';
+contentText.href = 'https://www.google.hu/';
+contentText.innerHTML = 'Example - Jump to Google';
+submittedText.innerHTML = 'submitted 1 month ago by anonymous';
+modifyButton.className = 'modify';
+modifyButton.innerHTML = 'modify';
+deleteButton.className = 'delete';
+deleteButton.innerHTML = 'delete';
 
 post.appendChild(voteContainer);
 voteContainer.appendChild(upvoteButton);
-voteCounter.innerHTML = score;
+voteCounter.innerHTML = 0;
 voteContainer.appendChild(voteCounter);
 voteContainer.appendChild(downvoteButton);
 
 post.appendChild(contentContainer);
+contentContainer.appendChild(contentText);
+contentContainer.appendChild(submittedText);
+contentContainer.appendChild(modifyButton);
+contentContainer.appendChild(deleteButton);
 
 postList.appendChild(post);
 
