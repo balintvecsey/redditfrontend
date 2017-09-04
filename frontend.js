b959db5569fbf1c80fac4f0591616487de4125c0
balintvecsey <balint.vecsey@gmail.com>
2017-09-04 12:47:21 +0200

    add the skeleton of the list element

--- frontend.js
+++ frontend.js
@@ -1,1 +1,17 @@
 'use strict';
+
+let postList = document.querySelector('ol');
+let post = document.createElement('li');
+let voteContainer = document.createElement('div');
+let contentContainer = document.createElement('div');
+
+voteContainer.className = 'vote container';
+contentContainer.className = 'content container';
+
+post.appendChild(voteContainer);
+post.appendChild(contentContainer);
+postList.appendChild(post);
+
+console.log(voteContainer);
+console.log(contentContainer);
+console.log(post);
