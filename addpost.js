7269a3efc516e2f6f4a96d67ea6627fa6c8ea68d
balintvecsey <balint.vecsey@gmail.com>
2017-09-06 19:52:33 +0200

    add posting function and send message to the server

--- addpost.js
+++ addpost.js
@@ -1,1 +1,18 @@
 'use strict';
+
+let url = document.querySelector('input[name="url"]');
+let title = document.querySelector('input[name="title"]');
+let submitButton = document.querySelector('button');
+let addNewPostReq = new XMLHttpRequest();
+
+let newPost = function() {
+  addNewPostReq.onreadystatechange = function functionName() {
+    window.location.href = 'index.html';
+  };
+
+  addNewPostReq.open('POST', 'http://localhost:8080/posts');
+  addNewPostReq.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
+  addNewPostReq.send(JSON.stringify({title: title.value, href: url.value}));
+}
+
+submitButton.addEventListener('click', newPost);
