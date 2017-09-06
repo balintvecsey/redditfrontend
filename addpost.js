8f091335011876a15ef479eec27174168b205452
balintvecsey <balint.vecsey@gmail.com>
2017-09-06 23:29:15 +0200

    redirect to index after everything is cool

--- addpost.js
+++ addpost.js
@@ -8,14 +8,16 @@ let addNewPostReq = new XMLHttpRequest();
 let newPost = function() {
   if(title.value !== '' && url.value !== '') {
     addNewPostReq.onreadystatechange = function functionName() {
-      window.location.href = 'index.html';
+      if(addNewPostReq.readyState === 4 && addNewPostReq.status === 200) {
+        window.location.href = 'index.html';  
+      }
     };
 
     addNewPostReq.open('POST', 'http://localhost:8080/posts');
     addNewPostReq.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
     addNewPostReq.send(JSON.stringify({title: title.value, href: url.value}));
   } else {
-    alert('You need to fill the form to submit!');  
+    alert('You need to fill the form to submit!');
   }
 }
 
